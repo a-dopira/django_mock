83827b8a75a61449c81424c2860353f5f876c42f
Anton D <doublescreen50@gmail.com>
2023-09-26 14:12:24 +0300

    notes styles

--- notes_app/notes/views.py
+++ notes_app/notes/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def simple_view(request):
-    return HttpResponse('<h1>Here is simple view with 1 h1 header</h1>')
+    context = {
+        'title': 'Main page is here'
+    }
+    return render(request, 'notes/notes.html', context=context)
