711a0e47f094f2a6ad128e864643552014424e92
Anton D <doublescreen50@gmail.com>
2023-10-18 21:59:16 +0300

    minor fixes

--- notes_app/notes/forms.py
+++ notes_app/notes/forms.py
@@ -4,7 +4,11 @@ from .models import Category, Note
 
 
 class NoteForm(forms.ModelForm):
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label=None)
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        empty_label=None,
+        widget=forms.Select(attrs={'class': 'custom-select-class'})
+    )
 
     class Meta:
         model = Note
@@ -13,4 +17,9 @@ class NoteForm(forms.ModelForm):
 
 class CategoryFilterForm(forms.Form):
     category_choices = Category.objects.all().values_list('id', 'title')
-    category = forms.ChoiceField(choices=category_choices, required=False, label='Оберіть категорію')
+    category = forms.ChoiceField(
+        choices=category_choices,
+        required=False,
+        label='Оберіть категорію',
+        widget=forms.Select(attrs={'class': 'custom-select-class'})
+    )
